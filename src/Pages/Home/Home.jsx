e5e71d56fc152031ba2a168a7b8992c12f828606
jlnko <jlnko@hotmail.fr>
2024-05-08 21:26:43 +0200

    creation du projet firebase, creation du fichier de config de firebase.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import {auth} from '../../db/firebase'
+import {Navigate} from 'react-router-dom'
+
 
 export default function Home(){
   const [isSignUpActive, setSignUpActive] = useState(false)
@@ -6,7 +10,9 @@ export default function Home(){
   const handleFormChange = () => {
     setSignUpActive(!isSignUpActive)
   }
-
+  const handleSignUp = () => {
+    if(!email || !password) return
+  }
 
   return (
    <section className="w-full h-screen bg-slate-900 flex items-center justify-center">
@@ -20,10 +26,8 @@ export default function Home(){
         <label htmlFor="password" className="text-slate-900">Password</label>
         <input type="password" name="password" id="password" className="h-10 border border-slate-900 rounded p-4" />
     
-        
-    
-        {isSignUpActive &&  <button type="submit" className="bg-slate-900 px-3 py-1.5 text-white my-3 rounded hover:bg-blue-700">Sign up</button>}
-        {!isSignUpActive &&  <button type="submit" className="bg-slate-900 px-3 py-1.5 text-white my-3 rounded hover:bg-blue-700">Sign In</button>}
+        {isSignUpActive &&  <button onClick={handleSignUp} type="submit" className="bg-slate-900 px-3 py-1.5 text-white my-3 rounded hover:bg-blue-700">Sign up</button>}
+        {!isSignUpActive &&  <button onClick={handleSignIn} type="submit" className="bg-slate-900 px-3 py-1.5 text-white my-3 rounded hover:bg-blue-700">Sign In</button>}
 
         {isSignUpActive && 
           <a href="#" onClick={handleFormChange} className="text-red-500 hover:text-red-900">Login</a>
